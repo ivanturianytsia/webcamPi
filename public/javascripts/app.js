b79767b34f88f03da139885500d15ad396057e21
sirivan <vanyaturianitsa@gmail.com>
2015-09-23 01:29:35 +0300

    completed 1

---
+++ public/javascripts/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('app', []);
+
+app.factory('Socket', function($rootScope) {
+    var socket = io.connect();
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
+
+app.controller('main', ['$scope', 'Socket',
+    function($scope, Socket) {
+        $scope.start = true;
+        $scope.streamSrc = '';
+        $scope.startStream = function() {
+            socket.emit('start-stream');
+        }
+        socket.on('live-stream', function(url) {
+            $scope.streamSrc = url;
+            $scope.start = false;
+        });
+    }
+]);
